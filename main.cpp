c94924eedb5968a673b70bc7e73a383a06457866
Alan Sampson <alansamps@gmail.com>
2020-06-09 20:45:29 +0000

    dynamic 2D array.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <iomanip>
 #include <string>
 #include <memory>
+#include <utility>
+#include <type_traits>
+#include <typeinfo>
+#include <cxxabi.h>
 
 int main() {
   std::cout << "Hello World!" << std::endl;
@@ -11,6 +15,67 @@ int main() {
   std::cout << (s_p ? "true" : "false") << std::endl;
   s_p.release();
   std::cout << (s_p ? "true" : "false") << std::endl;
+
+
+  size_t const M = 2;
+  size_t const N = 2;
+
+  std::cout << std::endl;
+
+  // allocate (no initializatoin)
+  auto array = new double[M][N];
+
+  // pollute the memory
+  array[0][0] = 2;
+  array[1][0] = 3;
+  array[0][1] = 4;
+  array[1][1] = 5;
+
+  for (size_t r = 0; r < M; ++r) {
+    for (size_t c = 0; c < N; ++c) {
+      std::cout << "array[" << r << "][" << c << "]: "
+                << array[r][c]
+                << ' ';
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  // re-allocate, probably will fetch the same memory block
+  // (not portable)
+  delete [] array;
+  array = new double[M][N];
+
+  // show that memory is not initialized
+  for (size_t r = 0; r < M; ++r) {
+    for (size_t c = 0; c < N; ++c) {
+      std::cout << "array[" << r << "][" << c << "]: "
+                << array[r][c]
+                << ' ';
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  // the proper way to zero-initialize the array
+  array = new double[M][N]();
+
+  // show the memory is initialized
+  for (size_t r = 0; r < M; ++r) {
+    for (size_t c = 0; c < N; ++c) {
+      std::cout << "array[" << r << "][" << c << "]: "
+                << array[r][c]
+                << ' ';
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  int info;
+  std::cout << abi::__cxa_demangle(typeid(array).name(), 0, 0, &info)
+            << std::endl;
+
+  delete [] array;
 
   return 0;
 }
